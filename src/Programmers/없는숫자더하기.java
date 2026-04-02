77d4b32c2bbd4a45c9e01a3b51227dabc7c727f4
DonggunSe0 <cloudhenry1@gmail.com>
2026-04-02 17:22:03 +0900

    없는 숫자 더하기_lv1

---
+++ src/Programmers/없는숫자더하기.java
@@ -0,0 +1,20 @@
+package Programmers;
+//lv1 월간 코드 챌린지
+class 없는숫자더하기 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        for(int i = 1; i < 10; i ++){
+            boolean found = false; // numbers 배열에서 i가 있는지 확인하는 플래그
+            for(int j = 0; j <numbers.length; j++ ){
+                if(i == numbers[j]){
+                    found = true; // i가 numbers 배열에 존재하면 found를 true로 설정하고 루프 종료
+                    break;
+                }
+            }
+            if(!found){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}

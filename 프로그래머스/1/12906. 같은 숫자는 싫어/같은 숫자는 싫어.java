ceb77dbeb331114fe5f23634aa50b2dc42417e5a
DonggunSe0 <cloudhenry1@gmail.com>
2026-05-09 01:33:26 +0900

    Update 같은 숫자는 싫어.java

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,11 +2,42 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        int[] answer = {};
-        
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (list.isEmpty() || list.get(list.size() - 1) != arr[i]) {
+                list.add(arr[i]);
+            }
+        }
+
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
 
         return answer;
-    }
+            
+            
+            
+            
+//         Deque<Integer> deq = new ArrayDeque<>();
+        
+//         for(int i = 0; i<arr.length; i++){
+//             if(!deq.isEmpty() && arr[i] == deq.peekLast()){
+//                 continue;
+//             }else{
+//                 deq.addLast(arr[i]);
+//             }
+//         }
+        
+//          int[] answer = new int[deq.size()];
+        
+//         for(int i = 0; i<answer.length; i++){
+//             answer[i] = deq.pollFirst();
+//         }
+
+//         return answer;
+//     }
+}
 }
